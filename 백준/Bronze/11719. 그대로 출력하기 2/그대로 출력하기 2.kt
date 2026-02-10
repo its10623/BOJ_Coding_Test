507b518c803dc3a02378a77a03c53ae597f52d99
Be hungry <92930071+its10623@users.noreply.github.com>
2026-02-10 16:18:13 +0900

    [Bronze III] Title: 그대로 출력하기 2, Time: 84 ms, Memory: 14152 KB -BaekjoonHub

--- 백준/Bronze/11719. 그대로 출력하기 2/그대로 출력하기 2.kt
+++ 백준/Bronze/11719. 그대로 출력하기 2/그대로 출력하기 2.kt
@@ -1,7 +1,8 @@
 fun main() {
-
+    val sb = StringBuilder()
     while (true) {
         val input = readlnOrNull() ?: break
-        println(input)
+        sb.appendLine(input)
     }
+    println(sb.toString())
 }
