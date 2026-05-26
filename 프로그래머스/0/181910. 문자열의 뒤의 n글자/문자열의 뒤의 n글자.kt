f9ebab1d2791d4e292ef94ff4f2ce58aa4237e2f
Be hungry <92930071+its10623@users.noreply.github.com>
2026-05-26 11:18:36 +0900

    Delete 프로그래머스/0/181910. 문자열의 뒤의 n글자 directory

--- 프로그래머스/0/181910. 문자열의 뒤의 n글자/문자열의 뒤의 n글자.kt
+++
@@ -1,9 +0,0 @@
-class Solution {
-    fun solution(my_string: String, n: Int): String {
-        var answer: String = ""
-        
-        val strLen = my_string.length
-        answer = my_string.substring(strLen - n)
-        return answer
-    }
-}
