8072b7c5428f1a5df0cd8948d7bef996ecdbf4e7
Be hungry <92930071+its10623@users.noreply.github.com>
2026-05-26 15:48:32 +0900

    [level 0] Title: 5명씩, Time: 9.78 ms, Memory: 65.5 MB -BaekjoonHub

--- 프로그래머스/0/181886. 5명씩/5명씩.kt
+++ 프로그래머스/0/181886. 5명씩/5명씩.kt
@@ -2,11 +2,7 @@ class Solution {
     fun solution(names: Array<String>): Array<String> {
         var answer: Array<String> = arrayOf<String>()
         
-        names.forEachIndexed { i, ch ->
-            if (i % 5 == 0 ) {
-                answer += ch
-            }
-        }
+        answer += names.filterIndexed { idx, _ -> idx % 5 == 0 }
         return answer
     }
 }
