bab2fddc5fc7db9c448b2a5dc55fd2ffd9d4be7f
Be hungry <92930071+its10623@users.noreply.github.com>
2026-05-26 11:15:04 +0900

    [level 0] Title: 배열 만들기 4, Time: 16.97 ms, Memory: 67 MB -BaekjoonHub

---
+++ 프로그래머스/0/181918. 배열 만들기 4/배열 만들기 4.kt
@@ -0,0 +1,21 @@
+class Solution {
+    fun solution(arr: IntArray): IntArray {
+        var stk = mutableListOf<Int>()
+        var i = 0
+
+        while(i < arr.size) {
+            if(stk.isEmpty()) {
+                stk.add(arr[i])
+                i++
+            } else {
+                if(stk.last() < arr[i]) {
+                    stk.add(arr[i])
+                    i++
+                } else {
+                    stk.remove(stk.last())
+                }
+            }
+        }
+        return stk.toIntArray()
+    }
+}
