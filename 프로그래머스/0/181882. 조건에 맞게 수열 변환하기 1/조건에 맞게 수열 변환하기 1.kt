6765601a01ed73d351177b081ffacd79e1da5519
Be hungry <92930071+its10623@users.noreply.github.com>
2026-06-01 16:43:16 +0900

    [level 0] Title: 조건에 맞게 수열 변환하기 1, Time: 27.93 ms, Memory: 162 MB -BaekjoonHub

--- 프로그래머스/0/181882. 조건에 맞게 수열 변환하기 1/조건에 맞게 수열 변환하기 1.kt
+++ 프로그래머스/0/181882. 조건에 맞게 수열 변환하기 1/조건에 맞게 수열 변환하기 1.kt
@@ -1,15 +1,9 @@
 class Solution {
     fun solution(arr: IntArray): IntArray {
-        var answer: IntArray = arr
-        
-        answer.forEachIndexed { i, ch ->
-            if (ch < 50 && ch % 2 !=0) {
-                answer[i] *= 2
-            } else if(ch >= 50 && ch % 2 == 0) {
-                answer[i] /= 2
-            }
-        }
-        
-        return answer
+        return arr.map { 
+            if (it < 50 && it % 2 != 0) it * 2
+            else if(it >= 50 && it % 2 == 0) it / 2
+            else it
+        }.toIntArray()
     }
 }
