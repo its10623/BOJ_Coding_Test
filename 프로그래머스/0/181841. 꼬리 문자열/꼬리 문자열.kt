1474bb2971e7e1ab3b58306d2dbcb736fa0513fc
Be hungry <92930071+its10623@users.noreply.github.com>
2026-06-30 16:25:06 +0900

    [level 0] Title: 꼬리 문자열, Time: 11.08 ms, Memory: 60.1 MB -BaekjoonHub

--- 프로그래머스/0/181841. 꼬리 문자열/꼬리 문자열.kt
+++ 프로그래머스/0/181841. 꼬리 문자열/꼬리 문자열.kt
@@ -1,12 +1,8 @@
 class Solution {
     fun solution(str_list: Array<String>, ex: String): String {
         var answer: String = ""
-        val parse = str_list.filterNot { it.contains(ex) }
+        answer = str_list.filterNot { it.contains(ex) }.joinToString("")
         
-        parse.indices.forEach {
-            answer += parse[it]
-        }
-
         return answer
     }
 }
