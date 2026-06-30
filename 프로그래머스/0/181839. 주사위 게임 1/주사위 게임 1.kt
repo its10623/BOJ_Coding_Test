22b8f648e6a2ab15b63ae2beb27b80374c954285
Be hungry <92930071+its10623@users.noreply.github.com>
2026-06-30 16:30:46 +0900

    [level 0] Title: 주사위 게임 1, Time: 0.02 ms, Memory: 58.1 MB -BaekjoonHub

---
+++ 프로그래머스/0/181839. 주사위 게임 1/주사위 게임 1.kt
@@ -0,0 +1,17 @@
+import kotlin.math.abs
+
+class Solution {
+    fun solution(a: Int, b: Int): Int {
+        var answer: Int = 0
+        
+        if(a % 2 != 0 && b % 2 != 0) {
+            answer = a*a + b*b
+        } else if(a % 2 != 0 || b % 2 != 0) {
+            answer = 2 * (a + b)
+        } else {
+            answer = abs(a - b)
+        }
+        
+        return answer
+    }
+}
