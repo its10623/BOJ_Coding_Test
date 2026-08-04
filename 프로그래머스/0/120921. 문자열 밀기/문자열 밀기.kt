862d73000d519d89611a26b3d4aab0f421b60af7
Be hungry <92930071+its10623@users.noreply.github.com>
2026-08-04 23:38:55 +0900

    [level 0] Title: 문자열 밀기, Time: 5.52 ms, Memory: 60.8 MB -BaekjoonHub

--- 프로그래머스/0/120921. 문자열 밀기/문자열 밀기.kt
+++ 프로그래머스/0/120921. 문자열 밀기/문자열 밀기.kt
@@ -1,14 +1,6 @@
 class Solution {
     fun solution(A: String, B: String): Int {
-        var answer: Int = 0
-
-        val bPlus = B + B
-        if (bPlus.contains(A)) {
-            answer = bPlus.indexOf(A)
-        } else {
-            answer = -1
-        }
-
+        var answer: Int = (B + B).indexOf(A)
         return answer
     }
 }
