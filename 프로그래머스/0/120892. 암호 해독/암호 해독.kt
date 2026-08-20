0ae73cc29b9f22e4dfa1f17e8306f2d733a29030
Be hungry <92930071+its10623@users.noreply.github.com>
2026-08-20 20:53:40 +0900

    [level 0] Title: 암호 해독, Time: 7.11 ms, Memory: 59.9 MB -BaekjoonHub

---
+++ 프로그래머스/0/120892. 암호 해독/암호 해독.kt
@@ -0,0 +1,11 @@
+class Solution {
+    fun solution(cipher: String, code: Int): String {
+        var answer: String = ""
+
+        for (i in code - 1 until cipher.length step code) {
+            answer += cipher[i]
+        }
+
+        return answer
+    }
+}
