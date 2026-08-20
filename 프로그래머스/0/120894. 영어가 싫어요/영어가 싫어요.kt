4780cc304ec076be3643483d8ff617062858e074
Be hungry <92930071+its10623@users.noreply.github.com>
2026-08-20 20:16:41 +0900

    [level 0] Title: 영어가 싫어요, Time: 5.56 ms, Memory: 59.9 MB -BaekjoonHub

---
+++ 프로그래머스/0/120894. 영어가 싫어요/영어가 싫어요.kt
@@ -0,0 +1,28 @@
+class Solution {
+    fun solution(numbers: String): Long {
+        var answer: String = ""
+        val list = mapOf(
+            "one" to 1,
+            "two" to 2,
+            "three" to 3,
+            "four" to 4,
+            "five" to 5,
+            "six" to 6,
+            "seven" to 7,
+            "eight" to 8,
+            "nine" to 9,
+            "zero" to 0
+        )
+        var string = ""
+        numbers.forEach { i ->
+            string += i
+
+            if (list.keys.contains(string)) {
+                answer += list[string]!!.toString()
+                string = ""
+            }
+        }
+
+        return answer.toLong()
+    }
+}
